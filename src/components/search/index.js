24f2361b2a8ff41e756485e90dedc628d6c5a186
Aleksandra <alexandrasiii@mail.ru>
2024-07-07 16:44:35 +0300

    some new

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import styles from "./index.module.css";
+import { SearchOutlined } from "@mui/icons-material";
 
 export const Search = () => {
 
     return (
         <>
             <div className={`${styles.searchField}`}>
-                <input type="text" />
+                <input type="text" placeholder="Ваш запрос" />
+                <SearchOutlined className={`${styles.search}`} style={{fontSize: 32}} />
             </div>
         </>
     )
